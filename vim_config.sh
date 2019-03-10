6ce52155c570203f088defca63250068eef1b23b
Gregory Cooke <gtcooke94@gmail.com>
2019-03-10 00:45:30 -0500

    Added symlink to init.vim

--- vim_config.sh
+++ vim_config.sh
@@ -1,13 +1,14 @@
 # Script to set up .vim, .vimrc, etc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-CONFIG_DIR='$HOME/repos/config/'
+CONFIG_DIR="$HOME/repos/config"
 
 mkdir ~/.vim
 mkdir ~/.vim/{bundle,autoload,swaps,backups}
 
-ln -s $CONFIG_DIR/.vimrc $HOME/.vim/.vimrc
-ln -s $HOME/.vim/.vimrc $HOME/.vimrc
+ln -s -f $CONFIG_DIR/.vimrc $HOME/.vim/.vimrc
+ln -s -f $HOME/.vim/.vimrc $HOME/.vimrc
+ln -s -f $HOME/.vimrc $HOME/.config/nvim
 
 pip install --user pylint
 pip install --user flake8
